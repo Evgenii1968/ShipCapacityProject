93f0970943a4723217abe0b294c0e37f7b49348e
histj <histj@Evgenii>
2025-01-19 17:18:09 +0900

    s

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,7 +8,7 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//s
 /**
  * Распределяем машины на 4 группы по округлённой площади:
  *   KEY:    round < 6
